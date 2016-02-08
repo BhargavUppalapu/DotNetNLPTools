4ae5ee242ba71eb42cb36f4733958627c3824486
Bhargav <bhargav.iiith@gmail.com>
2016-02-08 16:02:36 +0530

    Added Vowpal Wabbit code

---
+++ ClassificationTest/Classification.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Com.Research;
+//using Com.Research.ML.VowpalWabbit;
+using Com.Research.ML;
+
+
+namespace ClassificationTest
+{
+    public class Classification : IDisposable
+    {
+       private VowpalWabbit _vw;
+        private WordDictionary _dictionary;
+        private float _cutoff;
+
+        public Classification(string modelName, string dictionary, int dictionarySize, float cutoff)
+        {
+            //--quiet
+           _vw = new VowpalWabbit(string.Format("-t -i {0} --quiet", modelName));
+           _dictionary = new WordDictionary(dictionary, dictionarySize);
+           _cutoff = cutoff;
+        }
+
+
+        public void Predict(string features)
+        {
+            float predicted = _vw.Learn(features);
+        
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _vw.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+    }
+}
---
+++ ClassificationTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Com.Research.ML.TextExtraction;
+//using Com.Research.NLPCore;
+
+
+namespace ClassificationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string features = "";
+            var predictor = new DisclaimerPredict(@"D:\GitHub\DotNetNLPTools\trunk\resources\ModelFiles\VowpalWabbit\disclaimer.vw", @"D:\GitHub\DotNetNLPTools\trunk\resources\ModelFiles\VowpalWabbit\disclaimer.dict", 3000, 5.0f);
+            predictor.Predict(features);
+        
+        }
+    }
+}
