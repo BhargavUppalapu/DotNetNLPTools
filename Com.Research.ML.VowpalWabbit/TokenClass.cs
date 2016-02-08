d16ce8ad26b828e2a460ebf4f239802bb0f1472f
Bhargav <bhargav.iiith@gmail.com>
2016-02-08 16:08:29 +0530

    Added Vowpal Wabbit code

---
+++ Com.Research.ML.VowpalWabbit/TokenClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Com.Research.ML.VowpalWabbit
+{
+    class TokenClass
+    {
+
+        public static Regex NumberRegex = new Regex(
+           "^(CAD|C|US|EUR|HK)?[#$()+\\-,./\\\\]*\\d+([#$()+\\-,./\\\\]+\\d+)*[#$()+\\-,./\\\\XY]*$",
+           RegexOptions.IgnoreCase);
+        public static Regex DateRegex = new Regex(
+            "^("
+                + "\\d{1,2}/\\d{1,2}/\\d{4}"
+                + "|(\\dQ|Q\\d)?[F|C]Y\\d+[EA]?"
+                + "|\\dQ(\\d+[EA]?)?"
+                + "|(\\d+[\\-/])?(JAN(UARY)?|FEB(RUARY)?|MAR(CH)?|APR(IL)?|MAY|JUN(E)?|JUL(Y)?|AUG(UST)?|SEP(TEMBER)?|NOV(EMBER)?|DEC(EMBER)?)([\\-/]\\d+)?"
+            + ")$",
+            RegexOptions.IgnoreCase);
+
+        public static Regex URLRegex = new Regex("^(https?://|www\\.)|\\.(com|org|net|edu|gov)$", RegexOptions.IgnoreCase);
+        public static Regex EmailRegex = new Regex("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]+$", RegexOptions.IgnoreCase);
+        public static Regex AlphaNumericRegex = new Regex("^[A-Z]*\\d+([A-Z]+\\d+)*[A-Z]*$", RegexOptions.IgnoreCase);
+        public static Regex DelimiterRegex = new Regex("^[^A-Z\\d]+$", RegexOptions.IgnoreCase);
+        public static Regex NormalizeRegex = new Regex("[^A-Z]+", RegexOptions.IgnoreCase);
+
+
+        public static string NormalizeToken(string token)
+        {
+            if (string.IsNullOrEmpty(token))
+                return token;
+            else if (NumberRegex.Match(token).Success)
+                return "__number__";
+            else if (DateRegex.Match(token).Success)
+                return "__date__";
+            else if (AlphaNumericRegex.Match(token).Success)
+                return "__alphanumerical__";
+            else if (EmailRegex.Match(token).Success)
+                return "__email__";
+            else if (URLRegex.Match(token).Success)
+                return "__url__";
+            else if (DelimiterRegex.Match(token).Success)
+                return "__delimiter__";
+
+            return NormalizeRegex.Replace(token, string.Empty).ToLower();
+        }   
+    }
+}
---
+++ Com.Research.ML.VowpalWabbit/WordDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace Com.Research.ML.VowpalWabbit
+{
+     public class WordDictionary
+    {
+        private List<string> _words = new List<string>();
+
+        public WordDictionary(string path, int size)
+        {
+            _words = LoadDictionary(path, size);
+        }
+
+        public IEnumerable<string> Filter(IEnumerable<string> tokens)
+        {
+            var tokensSet = new HashSet<string>(tokens);
+            for (int i = 0; i < _words.Count; ++i)
+                if (tokensSet.Contains(_words[i]))
+                    yield return _words[i];
+        }
+
+        public BitArray ToFeaturesVector(IEnumerable<string> tokens)
+        {
+            var result = new BitArray(_words.Count);
+            var tokensSet = new HashSet<string>(tokens);
+
+            for (int i = 0; i < _words.Count; ++i)
+                if (tokensSet.Contains(_words[i]))
+                    result.Set(i, true);
+
+            return result;
+        }
+
+        private static List<string> LoadDictionary(string path, int maxSize)
+        {
+            return File.ReadAllLines(path)
+                .Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => s.Split(' '))
+                .Select(kv => kv[1])
+                .Take(maxSize)
+                .ToList();
+        }
+    }
+}
