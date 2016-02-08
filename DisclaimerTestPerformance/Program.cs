a5144716988d606e9f98c7925561f0878af6fe89
Bhargav <bhargav.iiith@gmail.com>
2016-02-08 16:12:36 +0530

    Added Vowpal Wabbit code

---
+++ DisclaimerTestPerformance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Com.Research.ML.TextExtraction;
+
+
+namespace DisclaimerTestPerformance
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            string features = "";
+            var predictor = new DisclaimerPredict(@"D:\GitHub\DotNetNLPTools\trunk\resources\ModelFiles\VowpalWabbit\disclaimer.vw", @"D:\GitHub\DotNetNLPTools\trunk\resources\ModelFiles\VowpalWabbit\disclaimer.dict", 3000, 5.0f);
+            predictor.Predict(features);
+        }
+    }
+}
